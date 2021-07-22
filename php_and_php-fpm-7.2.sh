873f156dfe25026451f5be30ea5e72f0e5736c9c
varshajpatil <57457219+varshajpatil@users.noreply.github.com>
2021-07-22 22:13:17 +0530

    Update php_and_php-fpm-7.2.sh

--- php_and_php-fpm-7.2.sh
+++ php_and_php-fpm-7.2.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 sudo yum -y install epel-release
 sudo yum-config-manager --enable remi-php72
-sudo yum install php
+sudo yum install php -y
 php -v
 sudo yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
-sudo yum install yum-utils
+sudo yum install yum-utils -y
 sudo yum-config-manager --enable remi-php72
-sudo yum install php72
-sudo yum install php72-php-fpm php72-php-gd php72-php-json php72-php-mbstring php72-php-mysqlnd php72-php-xml php72-php-xmlrpc php72-php-opcache
+sudo yum install php72 -y
+sudo yum -y install php72-php-fpm php72-php-gd php72-php-json php72-php-mbstring php72-php-mysqlnd php72-php-xml php72-php-xmlrpc php72-php-opcache
 sudo systemctl enable php72-php-fpm.service
 sudo systemctl status php72-php-fpm.service
 sudo systemctl start php72-php-fpm.service
