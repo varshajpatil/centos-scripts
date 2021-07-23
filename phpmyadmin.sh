cb81d124d85c986286bd6a4ed698cf14a6e48ebc
varshajpatil <57457219+varshajpatil@users.noreply.github.com>
2021-07-23 09:03:23 +0530

    Update phpmyadmin.sh

--- phpmyadmin.sh
+++ phpmyadmin.sh
@@ -1,9 +1,11 @@
 sudo yum install epel-release
 sudo yum install phpmyadmin -y
-sudo chgrp -R nginx /etc/phpMyAdmin
-sudo chown -R nginx /etc/phpMyAdmin
+sudo chgrp -R user /etc/phpMyAdmin
+sudo chown -R user /etc/phpMyAdmin
 sudo ln -sfn  /usr/share/phpMyAdmin /usr/share/nginx/html
 sudo nginx -t && sudo systemctl reload nginx
 sudo systemctl reload php72-php-fpm.service
 cd /var/lib/
-sudo chown -R nginx:nginx nginx/
+sudo chown -R user:user nginx/
+sudo chown -R root:user session/
+sudo systemctl reload php72-php-fpm.service
