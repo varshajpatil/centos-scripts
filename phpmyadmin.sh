db2667f298b9c83bde2f33b235f423a19ef05e37
varshajpatil <57457219+varshajpatil@users.noreply.github.com>
2021-07-23 09:05:11 +0530

    Update phpmyadmin.sh

--- phpmyadmin.sh
+++ phpmyadmin.sh
@@ -6,6 +6,8 @@ sudo ln -sfn  /usr/share/phpMyAdmin /usr/share/nginx/html
 sudo nginx -t && sudo systemctl reload nginx
 sudo systemctl reload php72-php-fpm.service
 cd /var/lib/
-sudo chown -R user:user nginx/
-sudo chown -R root:user session/
+##sudo chown -R user:user nginx/
+##sudo chown -R root:user session/
+cd /var/opt/remi/php72/lib/php
+sudo chown -R craft:craft session/
 sudo systemctl reload php72-php-fpm.service
